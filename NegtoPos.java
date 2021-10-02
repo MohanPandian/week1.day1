8a7317456013274274d52e063bfa4025383abb78
MohanPandian <91797104+MohanPandian@users.noreply.github.com>
2021-10-03 00:30:54 +0530

    Add files via upload

---
+++ NegtoPos.java
@@ -0,0 +1,14 @@
+package week1day1;
+
+public class NegtoPos {
+
+	public static void main(String[] args) {
+		
+		int number=-40;
+		if (number<=0) {
+			System.out.println("this number is -ve number so converting" +number+ " as positive number 40");
+
+		}
+		}
+
+}
---
+++ Numbercheck.java
@@ -0,0 +1,19 @@
+package week1day1;
+
+public class Numbercheck {
+
+	public static void main(String[] args) {
+		int N1=-35;
+		if (N1>0)
+		{
+			System.out.println("number is +ve");
+		}else if(N1<0)
+		{
+			System.out.println("number is -ve");
+		}else
+		{
+			System.out.println("number is neither positive or negative");
+		}
+	}
+
+}
